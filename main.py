9edbfabc198ab468b1bfce220f91958636cc124d
aysevarol <aysevarool.1804@gmail.com>
2023-09-14 21:38:58 +0300

    arrangement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import requests
+import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,19 +16,23 @@ async def home(request: Request):
 @app.get("/characters", response_class=HTMLResponse)
 async def characters(request):
     response = requests.get("https://rickandmortyapi.com/api/character")
-    characters = response.json()["results"]
-    return templates.TemplateResponse("characters.html", {"request": request, "character_list": characters})
+    character_list = response.json()["results"]
+    return templates.TemplateResponse("characters.html", {"request": request, "character": character_list})
 
 
 @app.get("/character/{character_id}", response_class=HTMLResponse)
 async def character(request: Request, character_id: int):
     response = requests.get(f"https://rickandmortyapi.com/api/character/{character_id}")
-    character = response.json()
-    return templates.TemplateResponse("character.html", {"request": request, "character": character})
+    character_only = response.json()
+    return templates.TemplateResponse("character.html", {"request": request, "character": character_only})
 
 
 @app.post("/search", response_class=HTMLResponse)
 async def search(request: Request, name: str = Form(...)):
     response = requests.get(f"https://rickandmortyapi.com/api/character/?name={name}")
-    characters = response.json()["results"]
-    return templates.TemplateResponse("characters.html", {"request": request, "character_list": characters})
+    character_ = response.json()["results"]
+    return templates.TemplateResponse("characters.html", {"request": request, "character": character_})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
