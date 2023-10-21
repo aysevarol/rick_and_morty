c6bcf5b50f9b24ce1bf346c95e5ba4a1cc1165d8
aysevarol <aysevarool.1804@gmail.com>
2023-10-21 20:05:53 +0300

    error 405

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ async def home(request: Request):
 
 @app.get("/characters", response_class=HTMLResponse)
 async def characters(request: Request):
-        response = requests.get("https://rickandmortyapi.com/api/character")
-        response.raise_for_status()
-        character_list = response.json()["results"]
-        return templates.TemplateResponse("characters.html", {"request": request, "character_list": character_list})
+    response = requests.get("https://rickandmortyapi.com/api/character")
+    response.raise_for_status()
+    character_list = response.json()["results"]
+    return templates.TemplateResponse("characters.html", {"request": request, "character_list": character_list})
 
 
 @app.get("/characters/{character_id}", response_class=HTMLResponse)
@@ -29,18 +29,14 @@ async def character(request: Request, character_id: int):
 
 
 @app.post("/search", response_class=HTMLResponse)
-async def search(request: Request, name: str = Form(...)):
-    name=input("Enter the character name")
+async def search_character(request: Request, name: str = Form(...)):
     response = requests.get(f"https://rickandmortyapi.com/api/character/?name={name}")
-    response.raise_for_status()
-    character_ = response.json()["results"]
-    if character_list:
-        print("Arama Sonuçları:")
-        for character in character_list:
-            print(f"Adı: {character['name']}, Durumu: {character['status']}")
+
+    if response.status_code == 200:
+        character_only = response.json()["results"]
+        return templates.TemplateResponse("search.html", {"request": request, "character": character_only})
     else:
-        print("Arama sonuçları bulunamadı.")
-    return templates.TemplateResponse("characters.html", {"request": request, "character_list": character_})
+        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Character not found"})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
